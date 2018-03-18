fd43be0d5609803270beb734db35881da06612df
colental <dm7718940@gmail.com>
2018-03-18 09:53:20 -0700

    03-18-18

--- db/tasks.sql
+++ db/tasks.sql
@@ -1,3 +1,46 @@
+/*
+
+Copyright (c) 2017 colental
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+    ,adPPYYba, 8b,dPPYba,   ,adPPYb,d8 88,dPPYba,   aa       aa
+    ""     `Y8 88P'   `"8a a8"    `Y88 88P'   `"8a  88       88
+    ,adPPPPP88 88       88 8b       88 88           8b       88
+    88,    ,88 88       88 "8a,   ,d88 88           "8a,   ,d88
+    `"8bbdP"Y8 88       88  `"YbbdP"Y8 88            `"YbbdP"Y8
+                            aa,    ,88               aa,    ,88
+                             "Y8bbdP"                 "Y8bbdP'
+
+                                                    88                          ,d
+                                                    88                          88
+     ,adPPYba,  ,adPPYb,d8  ,adPPYb,d8 8b,dPPYba,   88 ,adPPYYba, 8b,dPPYba,    88
+    a8P     88 a8"    `Y88 a8"    `Y88 88P'    "8a  88 ""     `Y8 88P'   `"8a MM88MMM
+    8PP8888888 8b       88 8b       88 88       d8  88 ,adPPPPP88 88       88   88
+    "8b,   ,aa "8a,   ,d88 "8a,   ,d88 88b,   ,a8"  88 88,    ,88 88       88   88
+     `"Ybbd8"'  `"YbbdP"Y8  `"YbbdP"Y8 88`YbbdP"'   88 `"8bbdP"Y8 88       88   88,
+                aa,    ,88  aa,    ,88 88                                       "Y888
+                 "Y8bbdP"    "Y8bbdP"  88
+
+
+*/
+
 CREATE TABLE IF NOT EXISTS `sessions` (
   `task` text NOT NULL,
   `session` text,
