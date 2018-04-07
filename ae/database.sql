7a6ae9c423de33e0e5e0520447f26f14345bf05b
colental <dm7718940@gmail.com>
2018-04-06 21:05:18 -0700

    04-06-18

--- ae/database.sql
+++ ae/database.sql
@@ -1,23 +1,40 @@
 #!/usr/bin/mysql
+#
+# The Angry Eggplant Project
+# https://github.com/colental/ae
+#
+# Copyright (c) 2017 Daniel Vega-Myhre
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
-/*
 
-	`angryeggplant`
-		type: database
-
-*/
+#	`angryeggplant`
+#		type: database
 
 
 CREATE DATABASE IF NOT EXISTS `ae`;
 use `ae`;
 
 
-/*
+#	`tbl_clients`
+#		type: table
 
-	`tbl_clients`
-		type: table
-		
-*/
 
 CREATE TABLE IF NOT EXISTS `tbl_clients` (
   `id` text NOT NULL,
@@ -35,12 +52,9 @@ CREATE TABLE IF NOT EXISTS `tbl_clients` (
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 
-/*
+#	`tbl_sessions`	
+#		type: table
 
-	`tbl_sessions`	
-		type: table
-
-*/
 
 CREATE TABLE IF NOT EXISTS `tbl_sessions` (
   `id` text NOT NULL,
@@ -54,12 +68,8 @@ CREATE TABLE IF NOT EXISTS `tbl_sessions` (
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 
-/*
-
-	`tbl_tasks`
-		type: table
-		
-*/
+#	`tbl_tasks`
+#		type: table
 
 CREATE TABLE IF NOT EXISTS `tbl_tasks` (
   `id` text NOT NULL,
@@ -73,23 +83,19 @@ CREATE TABLE IF NOT EXISTS `tbl_tasks` (
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 
-/*
-
-	`sp_addClient`
-		type: stored procedure	
-		usage: CALL sp_addClient(
-			client_id, 
-			public_ip, 
-			local_ip, 
-			mac_address, 
-			username, 
-			administrator, 
-			device, 
-			platform,
-			architecture
-		);
-
-*/
+#	`sp_addClient`
+#		type: stored procedure	
+#		usage: CALL sp_addClient(
+#			client_id, 
+#			public_ip, 
+#			local_ip, 
+#			mac_address, 
+#			username, 
+#			administrator, 
+#			device, 
+#			platform,
+#			architecture
+#		);
 
 
 
@@ -114,41 +120,37 @@ BEGIN
     public_ip,
     local_ip,
     mac_address,
-	username,
-	administrator,
-	device,
-	platform,
-	architecture
+    username,
+    administrator,
+    device,
+    platform,
+    architecture
     )
     values
     (
     client_id, 
-	client_public_ip,
-	client_local_ip,
-	client_mac_address,
-	client_username,
-	client_administrator,
-	client_device,
-	client_platform,
-	client_architecture,
+    client_public_ip,
+    client_local_ip,
+    client_mac_address,
+    client_username,
+    client_administrator,
+    client_device,
+    client_platform,
+    client_architecture
     );
 END$$
 DELIMITER ;
 
 
-/*
-
-	`sp_addSession`
-		type: stored procedure
-		usage: CALL sp_addSession(
-			session_id, 
-			client_id, 
-			session_key, 
-			public_key,
-			private_key
-		);
-
-*/
+#	`sp_addSession`
+#		type: stored procedure
+#		usage: CALL sp_addSession(
+#			session_id, 
+#			client_id, 
+#			session_key, 
+#			public_key,
+#			private_key
+#		);
 
 
 
@@ -183,19 +185,16 @@ END$$
 DELIMITER ;
 
 
-/*
-	
-	`sp_addTask`
-		type  stored procedure 
-		usage: CALL sp_addTask(
-				task_id, 
-				client_id, 
-				session_id, 
-				command, 
-				result
-			);
-			
-*/
+#	`sp_addTask`
+#		type  stored procedure 
+#		usage: CALL sp_addTask(
+#				task_id, 
+#				client_id, 
+#				session_id, 
+#				command, 
+#				result
+#			);
+
 
 DROP procedure IF EXISTS `ae`.`sp_addTask`; 
 DELIMITER $$
@@ -227,7 +226,3 @@ BEGIN
     );
 END$$
 DELIMITER ;
-
-
-
-
